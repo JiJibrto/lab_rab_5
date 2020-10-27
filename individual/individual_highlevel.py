b92cefe72de65cecc89c428c71d34b92841a9439
JiJibrto <jijibrto@gmail.com>
2020-10-27 22:28:55 +0300

    Add some changes to ind 3 and solution to High Level ind task

---
+++ individual/individual_highlevel.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+# Даны три слова. Напечатать неповторяющиеся в них буквы.
+
+if __name__ == '__main__':
+    inp_text = str(input("Введите три слова через пробел> "))
+    if inp_text.count(' ') != 2:
+        print("ВВЕДИТЕ ПОЖАЛУЙСТА ТРИ СЛОВА ЧЕРЕЗ ПРОБЕЛ!!", file=sys.stderr)
+        exit(1)
+    inp_text.replace(' ', '')
+    k = len(inp_text)
+    temp = ""
+
+    while k > 0:
+        k = k - 1
+        if inp_text.count(inp_text[k]) == 1:
+            temp += inp_text[k]
+
+    print("Буквы которые не повторялись в трех словах - " + temp)
